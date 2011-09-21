8a16956c15ab6ee945a87e27045ed3dd92e68998
Michal Koutný <koutny@necada.org>
2011-09-21 21:43:48 +0000

    http_touch pro debian

--- http_touch.sh
+++ http_touch.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
+set -x
 
 if [ $(git rev-parse --is-bare-repository) = true ]
 then
-	REPOSITORY_BASENAME=$(basename "$PWD") 
+	REPOSITORY_BASENAME=$(basename $(readlink -nf "$PWD"/..))
 	REPOSITORY_BASENAME=${REPOSITORY_BASENAME%.git}
 else
-	REPOSITORY_BASENAME=$(basename $(readlink -nf "$PWD"/..))
+	REPOSITORY_BASENAME=$(basename $(readlink -nf "$PWD"/../..))
 fi
+
+echo $REPOSITORY_BASENAME
 
 USER=user
 PWD=pwd
 
 AUTH=$(echo -n "$USER:$PWD" | base64)
 
-PATH="localhost:8000"
-HOST="/build/$REPOSITORY_BASENAME"
+HOST="localhost:8000"
+PATH="/build/$REPOSITORY_BASENAME"
 
 REQUEST="HEAD $PATH HTTP/1.0\r
 Host: $HOST\r
 Authorization: Basic $AUTH\r
 \r
 " 
-echo -ne "$REQUEST" | nc -q 1 ${1/:/ }
+#echo -ne "$REQUEST" | netcat -q 1 ${HOST/:/ }
+echo -ne "$REQUEST" | /bin/nc.traditional -q 1 ${HOST/:/ }
